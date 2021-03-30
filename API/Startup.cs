9dcdcd9b33d39488cea4d2be126a86612af4873a
ricardo-wells <wells_ricardo@yahoo.com>
2021-03-29 20:59:19 -0400

    End of Section3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,7 @@ namespace API
         {
 
             services.AddControllers();
+            services.AddCors();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
@@ -41,7 +42,7 @@ namespace API
               {
                   c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
               });
-                
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +58,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 

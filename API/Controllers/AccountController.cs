8a12b15bc37be69ca7a252cd13b77aa2ac104f9e
ricardo-wells <wells_ricardo@yahoo.com>
2021-04-02 13:07:26 -0400

    End Section 5

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -23,14 +23,14 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if (await UserExists(registerDto.Username))
+            if (await UserExists(registerDto.UserName))
             {
                 return BadRequest("Username is taken");
             }
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                Username = registerDto.Username.ToLower(),
+                Username = registerDto.UserName.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
 
